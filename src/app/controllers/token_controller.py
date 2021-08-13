3a05a80d84ef06f6ff5483f2c97113959c79621e
julianaskubs <julianaskubs@gmail.com>
2021-08-12 21:19:27 -0300

    removing token controller

--- src/app/controllers/token_controller.py
+++ src/app/controllers/token_controller.py
@@ -3,10 +3,4 @@ from flask import Blueprint, request, jsonify
 
 bp = Blueprint('token', __name__, url_prefix='/api')
 
-class TokenController:
-
-    @bp.route('/token', methods=['GET', 'POST'])
-    def convert_controller():
-        if request.method == "GET":
-            return jsonify({"result":"ok"})
-
+# TODO route to get jwt for API authentication
